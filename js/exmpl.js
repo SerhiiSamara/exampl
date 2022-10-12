277a88864351da5199511d80081ec2ef05c6896d
SerhiiSamara <ndaair@gmail.com>
2022-10-12 23:28:24 +0300

    Виправив нотифікацію у промісах

---
+++ js/exmpl.js
@@ -0,0 +1,10 @@
+// ------------Глибока деструктурізація---------------------
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   let {
+//     elements: { delay, step, amount },
+//   } = e.currentTarget;
+//   let amountEl = Number(amount.value);
+//   let stepEl = Number(step.value);
+//   let delayEl = Number(delay.value);
+// }
